19dc23abcb4870e66d14496c96c1ac59f1eb454e
[jarmo <toivanen@protonmail.com>
2026-08-29 21:06:32 +0300

    Add eslint & ci workflow

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+import js from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+
+export default defineConfig([
+  { ignores: ["dist/**"] },
+  {
+    files: ["**/*.ts"],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      tseslint.configs.recommendedTypeChecked,
+    ],
+    languageOptions: {
+      parserOptions: { projectService: true },
+    },
+  },
+  {
+    files: ["**/*.js"],
+    extends: [js.configs.recommended],
+  },
+  eslintConfigPrettier,
+]);
